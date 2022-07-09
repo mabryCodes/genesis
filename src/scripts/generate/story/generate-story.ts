78b50b0471c0d2ce04736f726840b8fd6d8a8562
Josh Mabry <jmabry@phase2technology>
2022-07-09 16:04:34 -0700

    nename directory

---
+++ src/scripts/generate/story/generate-story.ts
@@ -0,0 +1,74 @@
+/* eslint-disable valid-jsdoc */
+// eslint-disable-next-line unicorn/prefer-node-protocol
+import * as fs from 'fs'
+import path = require('path');
+import {storybookTemplate} from './templates'
+
+const config = {
+  customElementPath: 'test/mock-app/src/custom-element.json',
+  baseClass: 'LitElement',
+  // defaultNamespace: 'outline',
+}
+
+/**
+ * Creates story from custom element json
+ *
+ * @param {string} component - name of component
+ * @param {string} output - story output path, defaults to the same path as the component
+ * @param {string} path - path to custom-element.json. Defaults to '../src/custom-elements.json';
+ * @param {string} category - category of component defaults to 'Content'
+ */
+export const createStory = (
+  component: string,
+  output?: string,
+  nameSpace?: string,
+  category = 'Content',
+  customElementPath = config.customElementPath,
+  fullBleed = false,
+): void => {
+  const currDir = process.cwd()
+  const resolvedPath = path.resolve(currDir, customElementPath)
+
+  import(`${resolvedPath}`)
+  .then((customElements: any) => {
+    const componentData = customElements.tags.find(
+      (tag: { name: any }) => tag.name === component,
+    )
+    const attributes = componentData.attributes
+    const slots = componentData.slots
+    const cssVars = componentData.cssProperties
+    const namespace = component.split('-')[0]
+
+    // @todo make this dynamic and leverage the config
+    const nameSpaceFolder = namespace === 'outline' ? 'base' : namespace
+    const componentPath =
+        output ? path.resolve(currDir, output)          :
+          path.resolve(currDir, `src/components/${nameSpaceFolder}/${component}/generated-${component}.stories.ts`)
+
+    console.log('Creating story for', component, 'at', componentPath)
+
+    fs.writeFile(
+      componentPath,
+      storybookTemplate(
+        component,
+        {
+          attributes,
+          slots,
+          cssVars,
+        },
+        category,
+        fullBleed,
+      ),
+      error => {
+        if (error) {
+          console.error(error)
+        } else {
+          console.log(`${component} story created`)
+        }
+      },
+    )
+  })
+  .catch(error => {
+    console.error(error)
+  })
+}
---
+++ src/scripts/utility.ts
@@ -0,0 +1,69 @@
+/* eslint-disable valid-jsdoc */
+/**
+ * Converts from kebab-case to capital letter with spaces
+ * example 'outline-button' => 'Outline Button'
+ * @param {string} str
+ * @returns {string} str
+ */
+export const kebabCaseToTitleSpaceCase = (str: string): string => {
+  const arr = str.split('-')
+  return arr.map(el => el.charAt(0).toUpperCase() + el.slice(1)).join(' ')
+}
+
+/**
+ * Converts from kebab-case to capital letter with no spaces
+ * example 'outline-button' => 'OutlineButton'
+ * @param {string} str
+ */
+export const kebabCaseToTitleCase = (str: string): string => {
+  const arr = str.split('-')
+  return arr.map(el => el.charAt(0).toUpperCase() + el.slice(1)).join('')
+}
+
+/**
+ * Converts string from kebab-case to camelCase
+ * example 'outline-button' => 'outlineButton'
+ */
+export const kebabCaseToLowerCamelCase = (str: string): string => {
+  const arr = str.split('-')
+  const formattedArr = arr.map((el, i) => {
+    if (i === 0) {
+      return el
+    }
+
+    return el.charAt(0).toUpperCase() + el.slice(1)
+  })
+  return formattedArr.join('')
+}
+
+/**
+ * Converts string from camelCase to kebab-case
+ * example 'outline-button' => 'outlineButton'
+ * @param str
+ * @returns string
+ */
+export const camelCaseToKebabCase = (str: string): string => {
+  if (str !== str.toLowerCase()) {
+    str = str.replace(/[A-Z]/g, m => '-' + m.toLowerCase())
+  }
+
+  return str
+}
+
+/**
+ * Converts string from css variable to lowerCamelCase
+ * example '--outline-button' => 'outlineButton'
+ * @param str
+ * @returns
+ */
+export const cssVarCaseToLowerCamelCase = (str: string): string => {
+  const arr = str.split('-').filter(n => n)
+  const formattedArr = arr.map((el, i) => {
+    if (i === 0) {
+      return el
+    }
+
+    return el.charAt(0).toUpperCase() + el.slice(1)
+  })
+  return formattedArr.join('')
+}
